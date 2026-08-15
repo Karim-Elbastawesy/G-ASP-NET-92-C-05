c57be01d8df5f2f7bec4aafb0d91ccfa378192df
Karim-Elbastawesy <karimelbastawesy20@gmail.com>
2026-08-15 18:12:33 +0300

    Question02

--- G-ASP-NET-92-C#05/Program.cs
+++ G-ASP-NET-92-C#05/Program.cs
@@ -4,18 +4,33 @@
     {
         static void Main(string[] args)
         {
-            #region Question01
+            //#region Question01
+            //// ---------
+            //// Q: Add a private string password = "secret"; field to a Book class.
+            //// Try to print it from Main (outside the class). What happens, and why?
+            //// ---------
+
+            //Book book = new Book();
+
+            //// Console.WriteLine(book.password);
+            //// Why: This gives an error because private fields can only be accessed inside the Book class.
+
+            //// commit fix
+
+            //#endregion
+
+
+            #region Question02
             // ---------
-            // Q: Add a private string password = "secret"; field to a Book class.
-            // Try to print it from Main (outside the class). What happens, and why?
+            // Q: Add an internal int copiesInStock = 5; field to Book.
+            // Print it from Main. Does it compile? Why?
             // ---------
 
             Book book = new Book();
 
-            // Console.WriteLine(book.password);
-            // Why: This gives an error because private fields can only be accessed inside the Book class.
+            Console.WriteLine(book.copiesInStock);
 
-            // commit fix
+            // Why: It compiles because internal members can be accessed from anywhere in the same project.
 
             #endregion
 
@@ -23,6 +38,7 @@
         class Book
         {
             private string password = "secret";
+            internal int copiesInStock = 5;
 
         }
 
